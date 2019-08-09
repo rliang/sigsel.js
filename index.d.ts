0561b01770531da13ead58aafc420ad676d429e8
rliang <ricardoliang@gmail.com>
2019-08-09 01:39:29 -0300

    Add docs to typings file

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,32 @@
-export interface Signal<T> { readonly value: T, put(v: T): void };
+/**
+ * An unbuffered signal.
+ */
+export interface Signal<T> {
+  /**
+   * The signal's current value.
+   */
+  readonly value: T;
+  /**
+   * Updates the signal's value.
+   *
+   * @param v the new signal's value.
+   */
+  put(v: T): void;
+}
 
+/**
+ * Creates a signal.
+ *
+ * @param v the signal's initial value.
+ */
 export function signal<T = null>(v: T): Signal<T>;
 
-export function select<S extends Signal<unknown>[]>(...chs: S): Promise<S[number]>;
+/**
+ * Waits for a given set of signals to update.
+ *
+ * @param sigs the signals to await.
+ * @return p promise that resolves to the first updated signal.
+ */
+export function select<S extends Signal<unknown>[]>(
+  ...sigs: S
+): Promise<S[number]>;
